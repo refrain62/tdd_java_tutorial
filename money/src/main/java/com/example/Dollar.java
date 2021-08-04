3373953d1d65b58a168f6a3352980c2df5a9e311
nao <nao_h13@hotmail.com>
2021-08-04 22:48:19 +0900

    現時点のテストを通す

--- money/src/main/java/com/example/Dollar.java
+++ money/src/main/java/com/example/Dollar.java
@@ -9,8 +9,8 @@ public class Dollar {
         this.amount = amount;
     }
 
-    void times( int multiplier )
+    Dollar times( int multiplier )
     {
-        amount *= multiplier;
+        return new Dollar( amount * multiplier );
     }
 }
--- money/src/test/java/com/example/MoneyTest.java
+++ money/src/test/java/com/example/MoneyTest.java
@@ -15,10 +15,15 @@ public class MoneyTest
     @Test
     public void testMultiplication()
     {
+        Dollar product = null;
         Dollar five = new Dollar( 5 );
+        
+        // 2で掛けた場合のチェック
+        product = five.times( 2 );
+        assertEquals(10, product.amount);
 
-        five.times( 2 );
-
-        assertEquals(10, five.amount);
+        // 3で掛けた場合のチェック
+        product = five.times( 3 );
+        assertEquals(15, product.amount);
     }
 }
