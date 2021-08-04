09d61c0f681f40e61e6c3c4dace329e272316763
nao <nao_h13@hotmail.com>
2021-08-04 23:08:56 +0900

    Dollar をコピーして Franc　を生成

---
+++ money/src/main/java/com/example/Franc.java
@@ -0,0 +1,24 @@
+package com.example;
+
+
+public class Franc {
+    private int amount;
+    
+    Franc( int amount )
+    {
+        this.amount = amount;
+    }
+
+    Franc times( int multiplier )
+    {
+        return new Franc( amount * multiplier );
+    }
+
+    // イコール処理
+    public boolean equals( Object object )
+    {
+        Franc franc = (Franc)object;
+
+        return amount == franc.amount;
+    }
+}
