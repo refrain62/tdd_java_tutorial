01c9d76e80033b63585a6952b06f5a812e14626b
higashi <higashi@siu.co.jp>
2021-08-05 12:25:40 +0900

    為替の計算をBankに移設

--- money/src/main/java/com/example/Money.java
+++ money/src/main/java/com/example/Money.java
@@ -21,9 +21,9 @@ class Money implements Expression
         return new Sum( this, addend );
     }
 
-    public Money reduce( String to )
+    public Money reduce( Bank bank, String to )
     {
-        int rate = (currency.equals( "CHF" ) && to.equals("USD")) ? 2 : 1;
+        int rate = bank.rate( currency, to );
         return new Money( amount / rate, to );
     }
 
